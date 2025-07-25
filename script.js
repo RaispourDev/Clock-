bd67fa17b3d08515c884a661f999e0b1f8c64b3c
RaispourDev <mahmood.raispour@gmail.com>
2025-07-25 11:45:58 +0330

    Update JS file
    adding digital clock function and the operational btn to switch between them

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const digitalTime = document.querySelector('digital-time');
+const toggleBtn = document.querySelector('toggle-btn');
+const clockContainer = document.querySelector('.clock-container')
+
+let isDigital = false;
+
+function pad(n){
+    return n < 10 ? '0' + n : n;
+}
 
 function setDate(){
     const now = new Date();
-    
+
+//analog clock
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -16,8 +27,24 @@ function setDate(){
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) +90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+//digital clock
+
+    const hoursStr = pad(hour);
+    const minsStr = pad(mins);
+    const secsStr = pad(seconds);
+
+    digitalTime.textContent = `${hoursStr}:${minsStr}:${secsStr}`;
 }
 
-setInterval(setDate, 1000);
+//to refresh feed every second(keep up to date)
 
+setInterval(setDate, 1000);
 setDate();
+
+//switch between two clocks
+toggleBtn.addEventListener('click', () => {
+    isDigital = !isDigital;
+    clockContainer.classList.toggle('digital-mode');
+    toggleBtn.textContent = isDigital ? 'Digital' : 'analog';
+})
